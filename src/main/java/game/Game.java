59d99fb09f31631c584355c30626e61de5717528
Chan <1031297852@qq.com>
2021-03-10 20:35:12 -0800

    dev files

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,21 @@
+package game;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+import game.Map;
+
+public class Game extends JFrame {
+    public Game() {
+
+    }
+
+    public static void main(String[] arg) {
+        JFrame j = new JFrame("test");
+        j.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Map gameMap = new Map();
+        j.add(gameMap);
+        j.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        j.setVisible(true);
+    }
+}
---
+++ src/main/java/game/Map.java
@@ -0,0 +1,50 @@
+package game;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class Map extends JPanel {
+    String[][] level = {{"*","*","*","*","*","*","*","*","*","*", "*","*","*","*","*","*","*","*","*","*"},
+                        {"*","","","","","","","","","","","","","*","","","","","","s"},
+                        {"*","","","","","","","","","","","","","*","","*","*","*","*","*"},
+                        {"*","","","","","","","","","","","","","*","","*","","","","*"},
+                        {"*","","","","","","","","","","","","","","","*","*","*","","*"},
+                        {"*","","","","","","","","","","","","","","","","","","","*"},
+                        {"*","","","","","","","","","","","","","","*","*","*","*","","*"},
+                        {"*","","","","","","","","","","","","","","","","","*","","*"},
+                        {"s","","","","","","","","","","","","","","","","","","","*"},
+                        {"*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*","*"}};
+
+
+    public Map() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        this.setBackground(Color.WHITE);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(0, 0, 500, 500);
+        drawWalls(g);
+    }
+
+    private void drawWalls(Graphics g) {
+
+        for(int i = 0; i< level.length; i++) {
+            for(int j = 0; j<level[i].length; j++) {
+                if(level[i][j] == "*"){
+                    g.setColor(Color.BLACK);
+                    g.fillRect(j*25, i*25, 25, 25);
+                }
+                if(level[i][j] == "s"){
+                    g.setColor(Color.RED);
+                    g.fillRect(j*25, i*25, +25, +25);
+                }
+            }
+        }
+    }
+}
