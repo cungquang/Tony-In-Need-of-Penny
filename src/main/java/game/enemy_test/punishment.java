a7ed7b329314a80d37c7639b444735a2c08d7001
Shenyu Gu <shenyug@sfu.ca>
2021-03-24 12:23:07 -0700

    Enemy has punishment

---
+++ src/main/java/game/enemy_test/punishment.java
@@ -0,0 +1,21 @@
+package game.enemy_test;
+
+import java.lang.reflect.Constructor;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class punishment extends JFrame{
+    public punishment(){
+        JFrame pFrame = new JFrame();
+        JPanel pJPanel = new JPanel();
+        JLabel pJLabel = new JLabel("Game over");
+        pJPanel.add(pJLabel);
+        pFrame.add(pJPanel);
+        pFrame.setBounds(200, 200, 200, 200);
+        pFrame.setVisible(true);
+
+    }
+    
+}
